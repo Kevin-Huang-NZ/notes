d9937eedb92a37461d499e64f83c655792f3acc9
KevinHuang <kevinhuang325@gmail.com>
2017-02-21 21:22:20 +1300

    note

--- reactjs/budget/webpack.config.js
+++ reactjs/budget/webpack.config.js
@@ -1,58 +1,137 @@
-var path = require('path');
-var webpack = require('webpack');
-var HtmlwebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
 
-var ROOT_PATH = path.resolve(__dirname);
-var APP_PATH = path.resolve(ROOT_PATH, 'app');
-var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
+const sourcePath = path.join(__dirname, './client');
+const staticsPath = path.join(__dirname, './static');
 
-module.exports= {
-  entry: {
-    app: path.resolve(APP_PATH, 'index.jsx')
-  },
-  output: {
-    path: BUILD_PATH,
-    filename: 'bundle.js'
-  },
-  //enable dev source map
-  devtool: 'eval-source-map',
-  //enable dev server
-  devServer: {
-    historyApiFallback: true,
-    hot: true,
-    inline: true
-  },
-  //babel重要的loader在这里
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        include: APP_PATH,
-        use:[
-            'babel-loader'
-        ]
-      },
-      {
-        test: /\.scss$/,
-        // exclude: /node_modules/,
-        use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-        ]
-      },
-      {
-        test: /\.(png|jpg)$/,
-        loader: 'url?limit=40000'
-      }
-    ]
-  },
-  resolve: {
-      extensions: ['.js', '.jsx']
-  },
-  plugins: [
-    new HtmlwebpackPlugin({
-      title: 'My first react app'
-    })
-  ]
-}
+module.exports = function(env) {
+    const nodeEnv = env && env.prod ? 'production' : 'development';
+    const isProd = nodeEnv === 'production';
+
+    const plugins = [
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            minChunks: Infinity,
+            filename: 'vendor.bundle.js'
+        }),
+        new webpack.EnvironmentPlugin({
+            NODE_ENV: nodeEnv,
+        }),
+        new webpack.NamedModulesPlugin(),
+    ];
+
+    if (isProd) {
+        plugins.push(
+            new webpack.LoaderOptionsPlugin({
+                minimize: true,
+                debug: false
+            }),
+            new webpack.optimize.UglifyJsPlugin({
+                compress: {
+                    warnings: false,
+                    screw_ie8: true,
+                    conditionals: true,
+                    unused: true,
+                    comparisons: true,
+                    sequences: true,
+                    dead_code: true,
+                    evaluate: true,
+                    if_return: true,
+                    join_vars: true,
+                },
+                output: {
+                    comments: false,
+                },
+            })
+        );
+    } else {
+        plugins.push(
+            new webpack.HotModuleReplacementPlugin()
+        );
+    }
+
+    return {
+        devtool: isProd ? 'source-map' : 'eval',
+        context: sourcePath,
+        entry: {
+            js: './index.js',
+            vendor: ['react']
+        },
+        output: {
+            path: staticsPath,
+            filename: '[name].bundle.js',
+        },
+        module: {
+            rules: [{
+                    test: /\.html$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'file-loader',
+                        query: {
+                            name: '[name].[ext]'
+                        },
+                    },
+                },
+                {
+                    test: /\.css$/,
+                    exclude: /node_modules/,
+                    use: [
+                        'style-loader',
+                        'css-loader'
+                    ]
+                },
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: [
+                        'babel-loader'
+                    ],
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.webpack-loader.js', '.web-loader.js', '.loader.js', '.js', '.jsx'],
+            modules: [
+                path.resolve(__dirname, 'node_modules'),
+                sourcePath
+            ]
+        },
+
+        plugins,
+
+        performance: isProd && {
+            maxAssetSize: 100,
+            maxEntrypointSize: 300,
+            hints: 'warning',
+        },
+
+        stats: {
+            colors: {
+                green: '\u001b[32m',
+            }
+        },
+
+        devServer: {
+            contentBase: './client',
+            historyApiFallback: true,
+            port: 3000,
+            compress: isProd,
+            inline: !isProd,
+            hot: !isProd,
+            stats: {
+                assets: true,
+                children: false,
+                chunks: false,
+                hash: false,
+                modules: false,
+                publicPath: false,
+                timings: true,
+                version: false,
+                warnings: true,
+                colors: {
+                    green: '\u001b[32m',
+                }
+            },
+        }
+    };
+};
